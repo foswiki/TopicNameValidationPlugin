0e7df10659232e227d4dc2f7ff28985b23dee96d
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 11:16:47 +0000

    Item11808: while reverting perltidy build a new release
    git-svn-id: http://svn.foswiki.org/trunk/TopicNameValidationPlugin@14755 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicNameValidationPlugin.pm
+++ lib/Foswiki/Plugins/TopicNameValidationPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2008-2009 MichaelDaum http://michaeldaumconsulting.com
+# Copyright (C) 2008-2012 MichaelDaum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -27,7 +27,7 @@ use vars qw(
 );
 
 $VERSION = '$Rev$';
-$RELEASE = '1.0';
+$RELEASE = '1.1';
 $SHORTDESCRIPTION = 'Control naming of topics';
 $NO_PREFS_IN_TOPIC = 1;
 
